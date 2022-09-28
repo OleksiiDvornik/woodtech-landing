e0775852d6dea146f3f072273b2725f4844a0625
Oleksii Dvornik <alexey.dvronik@gmail.com>
2022-09-28 23:41:12 +0300

    Basic Gulp set up

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,76 @@
 // Импорт пакетов
 
 const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const sourcemap = require('gulp-sourcemaps');
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const concat = require('gulp-concat');
+const browserSync = require('browser-sync').create();
 
 // Пути к файлам
 
-const paths = {
-    
+const paths = { 
+    html: {
+        src: './src/*.html',
+        dest: './dist'
+    },
+    styles: {
+        src: './src/styles/scss/**/*.+(sass|scss)',
+        dest: './src/styles/css/'
+    },
+    scripts: {
+        src: './src/scripts/index.js',
+        dest: './src/scripts/'
+    },
+    images: {
+        src: './src/img/**/*.+(jpg|png)',
+        dest: './src/img/'
+    },
+    fonts: {
+        src: './src/fonts/*.*',
+        dest: './dist/fonts/'
+    }
 }
 
 // Задачи для разработки
+
+function styles() {
+    return gulp.src(paths.styles.src)
+        .pipe(sourcemap.init())
+        .pipe(sass())
+        .pipe(postcss([autoprefixer()]))
+        .pipe(sourcemap.write("."))
+        .pipe(gulp.dest(paths.styles.dest))
+        .pipe(browserSync.stream())
+}
+
+function scripts() {
+    return gulp.src(['./node_modules/swiper/swiper-bundle.js', paths.scripts.src])
+        .pipe(sourcemap.init())
+        .pipe(concat('bundle.js'))
+        .pipe(gulp.dest(paths.scripts.dest))
+        .pipe(browserSync.stream())
+}
 
 // Задачи для продакшена
 
 // Отслеживание изменений в файлах и запуск лайв сервера
 
+function watch() {
+    browserSync.init({
+        server: {
+            baseDir: "./src"
+        }
+    })
+    gulp.watch(paths.html.src).on('change', browserSync.reload);
+    gulp.watch(paths.styles.src, styles);
+    // gulp.watch(paths.scripts.src, scripts);
+}
+
 // Команды для запуска задач
+
+const dev = gulp.parallel(styles, scripts, watch);
+
+exports.default = dev;
+exports.convertImage = convertImage;
